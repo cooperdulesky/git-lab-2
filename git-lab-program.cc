548f0a96b61cf135a6697b18a473443cb08c0302
Cooper <cd319120@ohio.edu>
2023-01-24 11:50:44 -0500

    lab2

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Cooper Dulesky
+ *        Date: 1/24/23
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "hello Git!!" << endl;
+    return 0;
+}// main
